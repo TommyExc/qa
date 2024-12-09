c3079057c5f6f50d247184aca439e6087acdf630
TommyExc <157232438+TommyExc@users.noreply.github.com>
2024-12-09 21:48:38 +0300

    Update Main.java

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,12 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Создаем объекты типа Purchase с различными параметрами
         Purchase purchase1 = new Purchase(10, 150.50, 0.75);
         Purchase purchase2 = new Purchase(7, 200.00, 42.575);
         Purchase purchase3 = new Purchase(15, 120.25, 59.1);
 
-        // Выполняем расчеты и выводим результаты
         System.out.println("Первая покупка:");
         displayResults(purchase1);
 
